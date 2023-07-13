fc5c276c7747c5afba580640c7b856bb3d1d77fb
Pritheesh Panchmahalkar <pritheesh.panchmahalkar@match.com>
2023-07-13 12:19:41 -0500

    try left join on this

--- models/generated/airbyte_incremental/public/dbo_users.sql
+++ models/generated/airbyte_incremental/public/dbo_users.sql
@@ -9,18 +9,19 @@
 -- Final base SQL model
 -- depends_on: {{ ref('dbo_users_ab3') }}
 select
-    email,
-    ab_synced,
-    userid,
-    firstname,
-    createdt,
-    lastname,
-    ab_deleted,
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
+    a.email,
+    a.ab_synced,
+    a.userid,
+    a.firstname,
+    a.createdt,
+    a.lastname,
+    a.ab_deleted,
+    a._airbyte_ab_id,
+    a._airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at,
-    _airbyte_dbo_users_hashid
-from {{ ref('dbo_users_ab3') }}
+    a._airbyte_dbo_users_hashid
+from {{ ref('dbo_users_ab3') }} a
+left join {{this}} b on  a.userid = b.userid
 -- dbo_users from {{ source('public', '_airbyte_raw_dbo_users') }}
 where 1 = 1
 {{ incremental_clause('_airbyte_emitted_at', this) }}
